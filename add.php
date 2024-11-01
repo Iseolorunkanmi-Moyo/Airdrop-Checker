0146dcd2c8d98684058719459d9da174890dc443
Oluwatomisin <kanmitomi@gmail.com>
2024-11-01 12:06:22 +0100

    first commit

---
+++ add.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="./bootstrap.css">
+</head>
+<body>
+
+<div class="container">
+
+    <h1 class="text-center page-header">Add page</h1>
+
+    <div class="row">
+
+        <div class="col-1"></div>
+
+        <div class="col-9"><a class="btn btn-secondary" href="index.php">Back</a>
+
+            <form  method="post">
+                <input type="text" class="form-control mb-3 mt-3" name="name" placeholder="Name">
+                <input type="text" class="form-control mb-3" name="type" placeholder="Type">
+                <input type="text"class="form-control  mb-3" name="login" placeholder="Login Link">
+                <input type="text"class="form-control  mb-3" name="register" placeholder="Register Link">
+                <input type="submit" class="btn btn-primary" value="Save" name="Save" id="save">
+            </form>
+
+        </div>
+
+    </div>
+
+</div>
+
+<?php 
+    if(isset($_POST['Save'])){
+
+        $data = file_get_contents('data.json');
+        $data = json_decode($data,true);
+
+        if($_POST['name'] == "" ||$_POST['name'] == "" ||$_POST['name'] == "" ){
+            echo "<div id='warn' class='warn'><button id='close' class='btn-close mt-1'></button><div>Fill the inputs</div> </div>";
+        }
+
+        else{
+
+            $input = array(
+                "name" => $_POST['name'],
+                "type" => $_POST['type'],
+                "login" => $_POST['login'],
+                "register" => $_POST['register']
+            );
+    
+            $data[] = $input;
+            $data = json_encode($data, JSON_PRETTY_PRINT);
+            file_put_contents('data.json', $data);
+            // header('location: index.php');
+            // echo " <div class='alertDiv' id='alertDiv'><div class='alert-msg' id='alert-msg'><div>Form Submitted</div><img class='icon' src='./correct-success-tick-svgrepo-com.svg'><button id='closeAlert'>Ok</button></div></div>";
+        }
+        
+    }
+
+
+?>
+<script src="./redirect.js"></script>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="./bootstrap.css">
+    
+</head>
+<body>
+    <div class="container">
+
+    <h1 class="page-header text-center">Airdrop Checker</h1>
+    <div class="row">
+        <div class="col-12">
+            <a class="btn btn-primary" href="add.php">Add</a>
+            <table class="table table-bordered table-stripped mt-3 table-dark">
+                <thead>
+                    <th>Index</th>
+                    <th>Name</th>
+                    <th>Type</th>
+                    <th>Link</th>
+                    <th>Action</th>
+                </thead>
+
+                <tbody>
+                    
+                    <?php 
+                        $data = file_get_contents('data.json');
+                        $data = json_decode($data);
+
+                        $index = 0;
+                        foreach ($data as $row){
+                            echo "
+                            <tr>
+                                <td>".$index."</td>
+                                <td>".$row->name."</td>
+                                <td>".$row->type."</td>
+                                <td class='gap'><a class='btn btn-success ' href=".$row->login.">Check In</a> <a  class='btn btn-light ' href=".$row->register."> Register </a></td>
+                                <td>
+                                    <a href='edit.php?index=".$index." 'class='btn btn-warning btn-sm'>Edit</a>
+                                    <a href='delete.php?index=".$index." 'class='btn btn-danger btn-sm'>Delete</a>
+                                </td>
+                            </tr>
+                            ";
+
+                            $index++;
+                        }
+                    ?>
+                </tbody>
+            </table>
+        </div>
+    </div>
+         
+    </div>
+</body>
+</html>
